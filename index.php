49b683258d02d1f5baf94aed3beacdd9f16f20da
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2014-10-20 09:51:08 +0200

    Début de projet

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype>
+
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<link href="style.css" rel="stylesheet">
+		<title>Hello world</title>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="page-header">
+				<h1>Hello world</h1>
+			</div>
+		
+			<div class="row">
+				<div class="col-md-12">Hello world !</div>
+			</div>
+		</div>
+	</body>
+</html>
